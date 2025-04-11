f2f3b0e1caccad3d6f0f6738fca8ef54b6a9a07f
SnigdhaSucharita <snigdhasucharita7@gmail.com>
2025-04-11 10:53:51 +0530

    Added a unit test

--- tests/controller.unit.test.js
+++ tests/controller.unit.test.js
@@ -1,29 +1,35 @@
-const {
-  createNewUser
-} = require("../controller/controller");
+const { createNewUser } = require("../controller/controller");
 const { user: userModel } = require("../models");
+const {
+  validatePresenceOfUsernameAndEmail,
+  validateEmailFormat,
+} = require("../validations/index");
 const { doesUserExist } = require("../utils/utils");
 
+jest.mock("../validations/index.js");
+jest.mock("../utils/utils.js");
 jest.mock("../models");
-jest.mock("../utils/utils");
 
-describe("Controller unit tests", () => {
+describe("Controller - unit tests", () => {
   let req, res;
 
   beforeEach(() => {
     req = { body: {}, query: {} };
-    res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
+    res = { json: jest.fn(), status: jest.fn(() => res) };
   });
 
-  describe("createNewUser", () => {
-    it("should return 400 if username or email is missing", async() => {
-      req.body = { username: "", email: "" };
-      await createNewUser(req, res);
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.json).toHaveBeenCalledWith({ emptyFieldError: [ "Username is required", "Email is required" ] });
+  test("should return 400 if username or email is missing", async () => {
+    validatePresenceOfUsernameAndEmail.mockReturnValue([
+      "Username is required",
+      "Email is required",
+    ]);
+
+    req.body = { username: "", email: "" };
+    await createNewUser(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      emptyFieldError: ["Username is required", "Email is required"],
     });
   });
 });
