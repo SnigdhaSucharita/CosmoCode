8ccb60ea4b8c734c3fe731f2082830b6c7fd4633
SnigdhaSucharita <snigdhasucharita7@gmail.com>
2026-01-29 11:27:25 +0530

    bug fix

--- models/index.js
+++ models/index.js
@@ -24,14 +24,6 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     logging: false,
-    },
-
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
   });
 } else {
   // Local development
